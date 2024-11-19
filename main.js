d2c5997d93774f3152f901b5c843dbe9abe76f7b
SUSCookie12 <cookiestocky5@gmail.com>
2024-11-19 20:05:26 +0200

    Update 1

---
+++ main.js
@@ -0,0 +1,27 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+let mainWindow;
+
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+
+    // Load the `index.html` file in the same directory as the .exe
+    mainWindow.loadFile(path.join(__dirname, 'index.html'));
+
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    });
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
